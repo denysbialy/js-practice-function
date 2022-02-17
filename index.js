f2e7a7b7730ec4c558a5f74fb954f8034407cb1e
Denys <96307807+denysbialy@users.noreply.github.com>
2022-02-17 23:30:33 +0200

    Add modificate function

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
 // Задача 1
-const positiveNumber = function( num ){
-    if(num % 2 === 0){
-        return `Число ${num} четное`;
-    } else {
-        return `Число ${num} нечетное`;
-    }
-}
-console.log(positiveNumber(10))
+const positiveNumber = function (num) {
+  if (num % 2 === 0) {
+    return `Число ${num} четное`;
+  } else {
+    return `Число ${num} нечетное`;
+  }
+};
+console.log(positiveNumber(10));
 
 // Задача 2
 
-const minNumber = function(num1, num2){
-    if(num1 < num2){
-        return `Первое число ${num1} меньше второго числа ${num2}`
-    } else{
-        return `Первое число ${num1} больше второго числа ${num2}`
-    }
-}
-minNumber(10, 5);
+const minNumber = function (num1, num2) {
+  if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+};
 
-// Задача 3 
-
-const moneyForItem = function(money, item){
-    let result = 0;
-
-    if(money >= item){
-        ++result
-    }
-    return Boolean(result)
-}
-
-moneyForItem(10, 5)
+// Задача 3
+const isEnoughMoney = (money, item) => money >= item;
 
 // Задача 4
 
-const absolutlyNumeric = function(number){
-    if(number < 0){
-        return number *= -1
-    } else{
-        return number
-    }    
-}
-console.log(absolutlyNumeric(-50))
+const absolutlyNumeric = function (number) {
+  if (number < 0) {
+    return (number *= -1);
+  } else {
+    return number;
+  }
+};
+console.log(absolutlyNumeric(-50));
